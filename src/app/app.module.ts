940d5149c9c0511d610031f3ca6885f5a72ca57f
kvu-luong <luongkhanhvu1392@gmail.com>
2020-03-12 11:06:11 +0700

    add code product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDetailsComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/products.ts
@@ -0,0 +1,10 @@
+export class Product {
+    id: number;
+    name: string;
+    description: string;
+};
+export var products: Product[] = [
+    { id: 1, name: 'Iphone X' ,description: 'hehe haha test description'},
+    { id: 2, name: 'Samsung S9' , description: 'hehe haha test description'},
+    { id: 3, name: 'Sony Xperia XZs', description: '' },
+  ];
