889017be06db45249d92370736e77e77b1d6cce7
kvu-luong <luongkhanhvu1392@gmail.com>
2020-03-16 17:33:27 +0700

    add cart still error get price

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,31 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { ProductListComponent } from './product-list/product-list.component';
 
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductDetailsComponent,
-    ProductListComponent
+    ProductListComponent,
+    CartComponent,
+    TopBarComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
-    ])
+      { path: 'cart', component: CartComponent},
+      { path: 'shipping', component: ShippingComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
